d948f8dbd463f239d42f94b4ee081709e6d2a0a7
SanthiyaM-code <santhiya10082001@gmail.com>
2021-01-23 20:01:10 +0530

    getName setName methods for thread

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,14 +9,14 @@ public class Main {
                 } catch (Exception e) {
                 }
             }
-        });
+        },"Hi Thread");
         Thread t2=new Thread(()->{
             for (int i=0;i<5;i++)
             {
                 System.out.println("Hello");
                 try {Thread.sleep(1000); } catch(Exception e){}
             }
-        });
+        },"Hello Thread");
         t1.start();
         try {Thread.sleep(10); } catch(Exception e){}
         t2.start();
@@ -28,5 +28,16 @@ public class Main {
         t2.join();
         System.out.println(t1.isAlive());
         System.out.println("Byeee");
+
+        //Name ofthe Threads
+        System.out.println(t1.getName());
+        System.out.println(t2.getName());
+        //changing names of the thread
+        t1.setName("Hi");
+        t2.setName("Hello");
+        System.out.println(t1.getName());
+        System.out.println(t2.getName());
+
+
     }
 }
