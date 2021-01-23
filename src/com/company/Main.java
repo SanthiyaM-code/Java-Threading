685203c1abd94866efa688e20568df91e5cf896a
SanthiyaM-code <santhiya10082001@gmail.com>
2021-01-23 21:36:30 +0530

    synchronized keyword in Threads

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,51 +1,35 @@
 package com.company;
+
+
+
+class Counter
+{
+    int count;
+    public synchronized void increament()
+    {
+        count++;
+    }
+}
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Thread t1=new Thread(()->{
-            for (int i=0;i<5;i++) {
-                System.out.println("Hi");
-                try {
-                    Thread.sleep(1000);
-                } catch (Exception e) {
-                }
-            }
-        },"Hi Thread");
-        Thread t2=new Thread(()->{
-            for (int i=0;i<5;i++)
+    public static void main(String []args) throws Exception {
+        Counter c=new Counter();
+        Thread t1=new Thread(() -> {
+            for(int i=1;i<=1000;i++)
             {
-                System.out.println("Hello");
-                try {Thread.sleep(1000); } catch(Exception e){}
+                c.increament();
             }
-        },"Hello Thread");
+        });
+        Thread t2=new Thread(() -> {
+            for(int i=0;i<1000;i++)
+            {
+                c.increament();
+            }
+        });
         t1.start();
-        try {Thread.sleep(10); } catch(Exception e){}
         t2.start();
-
-        System.out.println(t1.isAlive());
-
         t1.join();
-
         t2.join();
-        System.out.println(t1.isAlive());
-        System.out.println("Byeee");
 
-        //Name ofthe Threads
-        System.out.println(t1.getName());
-        System.out.println(t2.getName());
-        //changing names of the thread
-        t1.setName("Hi");
-        t2.setName("Hello");
-        System.out.println(t1.getName());
-        System.out.println(t2.getName());
-
-        //priority
-        t1.setPriority(Thread.MAX_PRIORITY);
-        t2.setPriority(Thread.MIN_PRIORITY);
-
-        System.out.println(t1.getPriority());
-        System.out.println(t2.getPriority());
-
-
-
+        System.out.println("count "+c.count);
     }
 }
