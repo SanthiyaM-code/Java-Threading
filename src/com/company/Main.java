eabd8e7a9a1f50f13e0b8e811a18580fe3fd515e
SanthiyaM-code <santhiya10082001@gmail.com>
2021-01-23 19:56:07 +0530

    join method and isAlive methods in thread

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,6 @@
 package com.company;
-
 public class Main {
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Thread t1=new Thread(()->{
             for (int i=0;i<5;i++) {
                 System.out.println("Hi");
@@ -18,14 +16,17 @@ public class Main {
                 System.out.println("Hello");
                 try {Thread.sleep(1000); } catch(Exception e){}
             }
-
-
         });
-
-
         t1.start();
         try {Thread.sleep(10); } catch(Exception e){}
         t2.start();
 
+        System.out.println(t1.isAlive());
+
+        t1.join();
+
+        t2.join();
+        System.out.println(t1.isAlive());
+        System.out.println("Byeee");
     }
 }
