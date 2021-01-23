53f176ae91d6a2126bafc9c63e407b5eee0563c9
SanthiyaM-code <santhiya10082001@gmail.com>
2021-01-23 19:36:43 +0530

    Direct Thread class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,31 +3,24 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
-        Runnable obj1=()->
-
-        {
-                    for (int i=0;i<5;i++) {
-                        System.out.println("Hi");
-                        try {
-                            Thread.sleep(1000);
-                        } catch (Exception e) {
-                        }
-                    }
-
-        };
-        Runnable obj2=()-> {
-
-                for (int i=0;i<5;i++)
-                {
-                    System.out.println("Hello");
-                    try {Thread.sleep(1000); } catch(Exception e){}
+        Thread t1=new Thread(()->{
+            for (int i=0;i<5;i++) {
+                System.out.println("Hi");
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
                 }
+            }
+        });
+        Thread t2=new Thread(()->{
+            for (int i=0;i<5;i++)
+            {
+                System.out.println("Hello");
+                try {Thread.sleep(1000); } catch(Exception e){}
+            }
 
 
-        };
-        Thread t1=new Thread(obj1);
-        Thread t2=new Thread(obj2);
+        });
 
 
         t1.start();
