10f1db7da0496a1cc6eee345dbefa83c89e543c6
SanthiyaM-code <santhiya10082001@gmail.com>
2021-01-23 19:25:33 +0530

    Runnable interface

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,16 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Hi obj1=new Hi();
-        Hello obj2=new Hello();
+        Runnable obj1=new Hi();
+        Runnable obj2=new Hello();
+        Thread t1=new Thread(obj1);
+        Thread t2=new Thread(obj2);
 
-        obj1.start();
+
+        t1.start();
         try {Thread.sleep(10); } catch(Exception e){}
-        obj2.start();
+        t2.start();
 
     }
 }
-class Hi extends Thread
+class Hi implements Runnable
 {
     public void run()
     {
